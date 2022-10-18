db67ab950d95f3ea934f30ce1d2bfe1933575d6c
mt201064 <mt201064@fhstp.ac.at>
2022-10-18 13:24:39 +0200

    test

--- Backend/config.inc.php
+++ Backend/config.inc.php
@@ -2,7 +2,7 @@
 
 require('functions.inc.php');
 
-define("DB_HOST","mysql5");
+define("DB_HOST","mysql6");
 define("DB_NAME","db_mt201064_1");
 define("DB_USER","mt201064");
 define("DB_PW","4W8ETMCwJpym");
