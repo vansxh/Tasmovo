3eb42a9e7ee4a6c629259e02e5628f3868743c28
mt201064 <mt201064@fhstp.ac.at>
2022-11-21 14:45:00 +0100

    added count of tasks/finished tasks in profile component (frontend)

--- Frontend/src/app/pages/profile/profile.component.ts
+++ Frontend/src/app/pages/profile/profile.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ThemePalette} from '@angular/material/core';
 import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
 import {AuthenticationService} from "../../services/authentication/authentication.service";
+import {TaskService} from "../../services/task/task.service";
+import {GeneralService} from "../../services/general/general.service";
+import {Task} from "../../services/task/task";
 
 @Component({
   selector: 'app-profile',
@@ -10,17 +13,44 @@ import {AuthenticationService} from "../../services/authentication/authenticatio
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private auth: AuthenticationService) {
+  constructor(private auth: AuthenticationService, private taskService: TaskService, private general: GeneralService) {
   }
-  color: ThemePalette = 'primary';
+  /*color: ThemePalette = 'primary';
   mode: ProgressSpinnerMode = 'determinate';
-  value = 50;
+  value = 50;*/
+  allTasks!: Task[];
+  finishedTasks!: Task[];
+
 
   ngOnInit(): void {
+    this.taskService.getAllTasks().subscribe(
+      (data: any = []) => {
+        // get tasks from data
+        this.allTasks = <Task[]>data['data'];
+      },
+      (error: any = []) => {
+        if(error['error']['message']) {
+          alert(error['error']['message']);
+          return;
+        }
+        this.general.errorResponse(error['status']);
+      });
+
+    this.taskService.getFinishedTasks().subscribe(
+      (data: any = []) => {
+        // get tasks from data
+        this.finishedTasks = <Task[]>data['data'];
+      },
+      (error: any = []) => {
+        if(error['error']['message']) {
+          alert(error['error']['message']);
+          return;
+        }
+        this.general.errorResponse(error['status']);
+      });
   }
 
   logout() {
-    //console.log(this.service.getSession());
     this.auth.logout(['Logout']).subscribe((data: any = []) => {
       //console.log(data);
       if (data['error'] == false) {
