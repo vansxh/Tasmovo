5e39c66bf1f49f8900c16b6608b31150a21444ac
mt201006 <mt201006@fhstp.ac.at>
2022-11-28 12:15:36 +0100

    added Calendar Detail Component

--- Frontend/src/app/pages/calendar/calendar.component.ts
+++ Frontend/src/app/pages/calendar/calendar.component.ts
@@ -7,6 +7,7 @@ import {Router, Params, ActivatedRoute} from '@angular/router';
 import {DatePipe} from '@angular/common';
 import {LOCALE_ID} from '@angular/core';
 import {AuthenticationService} from 'src/app/services/authentication/authentication.service';
+import { Observable } from 'rxjs';
 import {
   isSameMonth,
   isSameDay,
@@ -18,6 +19,20 @@ import {
   endOfDay,
   format,
 } from 'date-fns';
+import {colors} from "@angular-devkit/build-angular/src/utils/color";
+import { map } from 'rxjs/operators';
+
+
+function getTimezoneOffsetString(date: Date): string {
+  const timezoneOffset = date.getTimezoneOffset();
+  const hoursOffset = String(
+    Math.floor(Math.abs(timezoneOffset / 60))
+  ).padStart(2, '0');
+  const minutesOffset = String(Math.abs(timezoneOffset % 60)).padEnd(2, '0');
+  const direction = timezoneOffset > 0 ? '-' : '+';
+
+  return `T00:00:00${direction}${hoursOffset}:${minutesOffset}`;
+}
 
 @Component({
   selector: 'app-calendar',
@@ -35,12 +50,28 @@ export class CalendarComponent {
   public openTasks!: Task[];
   activeDayIsOpen: boolean = true;
   insertEvent!: CalendarEvent;
+  events$!: Observable<CalendarEvent<{ task: Task }>[]>;
 
   constructor(private taskService: TaskService, private general: GeneralService, private router: Router, private route: ActivatedRoute, private datePipe: DatePipe, private authService: AuthenticationService) {}
 
   ngOnInit(): void {
-
-
+    /*this.events$=this.taskService.getAllTasks().pipe(
+      map(({ results }: { results: Task[] }) => {
+        return results.map((task: Task) => {
+          return {
+            title: task.task_name,
+            start: new Date(
+              task.deadline + getTimezoneOffsetString(this.viewDate)
+            ),
+            color: colors.yellow,
+            allDay: true,
+            meta: {
+              task,
+            },
+          };
+        });
+      })
+    );*/
   }
 
   changeToDayView() {
