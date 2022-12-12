cdae75069ddee26a13265ee52f4c3e3a88df040f
mt201064 <mt201064@fhstp.ac.at>
2022-12-12 09:26:20 +0100

    fixed bug of weekly average

--- Backend/classes/Stresstracking.class.php
+++ Backend/classes/Stresstracking.class.php
@@ -5,9 +5,11 @@ class Stresstracking
     function getWeeklyAvg($today){
         //SELECT AVG(t.stress_factor) AS 'Average' FROM User AS u INNER JOIN Task AS t ON u.UID = t.created_by WHERE u.UID = '292e9f36-b177-4e31-9abb-2dfd54aa1904' AND t.end_date >= ('2022-11-28'-7) AND t.end_date <= '2022-11-28'
         //SELECT AVG(t.stress_factor) AS 'Average' FROM User AS u INNER JOIN Task AS t ON u.UID = t.created_by WHERE u.UID = '292e9f36-b177-4e31-9abb-2dfd54aa1904' AND t.end_date >= DATE_SUB("2022-11-28", INTERVAL 7 DAY) AND t.end_date <= '2022-11-28'
-        $stmt = Database::getDb()->prepare("SELECT ROUND(AVG(t.stress_factor),2) AS 'Average' FROM User AS u INNER JOIN Task AS t ON u.UID = t.created_by WHERE u.UID = :UID AND t.end_date >= DATE_SUB(:today, INTERVAL 7 DAY) AND t.end_date <= :today");
+        $tomorrow = $today + 1;
+        $stmt = Database::getDb()->prepare("SELECT ROUND(AVG(t.stress_factor),2) AS 'Average' FROM User AS u INNER JOIN Task AS t ON u.UID = t.created_by WHERE u.UID = :UID AND t.end_date >= DATE_SUB(:today, INTERVAL 7 DAY) AND t.end_date < :tomorrow");
         $stmt->bindValue(":UID", $_SESSION['UID']);
         $stmt->bindValue(":today", $today);
+        $stmt->bindValue(":tomorrow", $tomorrow);
 
         $stmt->execute();
 
