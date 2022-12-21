fc62ae270419600d3c10de5ce487d27a53ae8411
mt201064 <mt201064@fhstp.ac.at>
2022-12-21 13:55:02 +0100

    reset daily stress level bug fix

--- Backend/classes/TimerReset.class.php
+++ Backend/classes/TimerReset.class.php
@@ -12,16 +12,18 @@ class TimerReset{
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if (strtotime($result['value']) <= $today){
+        if ($result['value'] <= $today){
             return;
+        }else{
+            $setting = Database::getDb()->prepare("UPDATE Settings SET value = :today WHERE record = :record");
+            $setting->bindValue(":today", $today);
+            $setting->bindValue(":record", self::lastResetDate);
+            $setting->execute();
+
+            $reset = Database::getDb()->prepare("UPDATE User SET daily_stresslevel = 0");
+            $reset->execute();
         }
 
-        $setting = Database::getDb()->prepare("UPDATE Settings SET value = :today WHERE record = :record");
-        $setting->bindValue(":today", $today);
-        $setting->bindValue(":record", self::lastResetDate);
-        $setting->execute();
 
-        $reset = Database::getDb()->prepare("UPDATE User SET daily_stresslevel = 0");
-        $reset->execute();
     }
 }
