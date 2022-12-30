9f86b662bf4988e06975b59411e2564c74955c83
mt201064 <mt201064@fhstp.ac.at>
2022-12-30 15:58:33 +0100

    changed to the db of host

--- Frontend/proxy.conf.js
+++ Frontend/proxy.conf.js
@@ -1,6 +1,6 @@
 const PROXY_CONFIG = {
   "/Backend": {
-      "target": "http://flock-1902.students.fhstp.ac.at",
+      "target": "https://www.tasmovo.at",
       "changeOrigin": true,
       "secure": false,
       "logLevel": "debug"
