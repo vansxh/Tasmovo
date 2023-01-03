682e027b65a957475ad16530d1214647e8206a1f
mt201063 <mt201063@fhstp.ac.at>
2023-01-03 11:48:52 +0100

    worked on area chart for stress level

--- Frontend/src/app/pages/insert-group/insert-group.component.ts
+++ Frontend/src/app/pages/insert-group/insert-group.component.ts
@@ -4,6 +4,8 @@ import {VisualizationService} from "../../services/visualization/visualization.s
 import {GeneralService} from "../../services/general/general.service";
 import {DatePipe} from "@angular/common";
 Chart.register(...registerables);
+import annotationPlugin from "chartjs-plugin-annotation";
+Chart.register(annotationPlugin);
 
 @Component({
   selector: 'app-insert-group',
@@ -35,8 +37,19 @@ export class InsertGroupComponent implements OnInit {
           this.days.push( d );
         }
 
+        // set labels and fill data Arrays with as much 0s as there are days
+        for(var i=0; i<this.days.length; i++) {
+          this.labels.push(this.days[i].toLocaleString('de-at', {  weekday: 'short' }));
+          this.dataEasy.push(0);
+          this.dataMedium.push(0);
+          this.dataDiff.push(0);
+        }
+
         // init stacked bar chart
-        this.initChart();
+        this.initBarChart();
+
+        // init area chart
+        this.initAreaChart();
 
       },
       (error: any = []) => {
@@ -49,15 +62,7 @@ export class InsertGroupComponent implements OnInit {
 
   }
 
-  initChart(): void {
-
-    // set labels and fill data Arrays with as much 0s as there are days
-    for(var i=0; i<this.days.length; i++) {
-      this.labels.push(this.days[i].toLocaleString('de-at', {  weekday: 'short' }));
-      this.dataEasy.push(0);
-      this.dataMedium.push(0);
-      this.dataDiff.push(0);
-    }
+  initBarChart(): void {
 
     // go through each day
     for(var i=0; i<this.days.length; i++) {
@@ -85,7 +90,7 @@ export class InsertGroupComponent implements OnInit {
       }
     }
 
-    const data = {
+    const barChartData = {
       labels: this.labels,
       datasets: [
         {
@@ -106,9 +111,9 @@ export class InsertGroupComponent implements OnInit {
       ]
     };
 
-    var myChart = new Chart("myChart", {
+    var barChart = new Chart("myBarChart", {
         type: 'bar',
-        data: data,
+        data: barChartData,
         options: {
           plugins: {
             title: {
@@ -131,6 +136,61 @@ export class InsertGroupComponent implements OnInit {
           }
         }
       });
+
+  }
+
+  initAreaChart(): void {
+
+    const areaChartData = {
+      labels: this.labels,
+      datasets: [
+        {
+          label: 'Stresslevel',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: {
+            target: {value: 60},
+            above: 'rgb(208, 199, 236)',
+            below: 'rgba(0,0,0,0)'
+          },
+          borderColor: 'rgb(99, 76, 154)',
+          tension: 0.1
+        },
+      ]
+    };
+
+    var areaChart = new Chart("myAreaChart", {
+      type: "line",
+      data: areaChartData,
+      options: {
+        plugins: {
+          filler: {
+            propagate: true
+          },
+          annotation: {
+            annotations: {
+              line1: {
+                type: 'line',
+                yMin: 60,
+                yMax: 60,
+                borderColor: 'rgb(48,35,91)',
+                borderWidth: 3,
+              },
+              label1: {
+                type: 'label',
+                xValue: 0.3,
+                yValue: 60-1,
+                content: ['Dein Stresslimit'],
+                font: {
+                  family: "'Ubuntu', sans-serif",
+                  size: 14,
+                },
+                color: 'rgb(48,35,91)',
+              }
+            }
+          }
+        },
+      }
+    });
 
   }
 
