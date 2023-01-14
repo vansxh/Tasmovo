ea1736e4948fd8e90a69b0e1391ddb798c3b4884
mt201064 <mt201064@fhstp.ac.at>
2023-01-14 23:51:04 +0100

    decode special characters calendar-detail component

--- Frontend/src/app/pages/calendar-detail/calendar-detail.component.ts
+++ Frontend/src/app/pages/calendar-detail/calendar-detail.component.ts
@@ -154,4 +154,8 @@ export class CalendarDetailComponent implements OnInit {
     dialogConfig.autoFocus = true;
     this.dialog.open(PopupFinishComponent, dialogConfig);
   }
+
+  decodeSpecialCharacters(str: string){
+    return this.general.decodeHtmlCharCodes(str);
+  }
 }
