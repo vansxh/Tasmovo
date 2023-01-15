0622932a5e316a662b9d976681d47a28c1cf4896
mt201064 <mt201064@fhstp.ac.at>
2023-01-15 16:29:42 +0100

    code cleanup stresstracking class

--- Backend/classes/Stresstracking.class.php
+++ Backend/classes/Stresstracking.class.php
@@ -2,9 +2,8 @@
 
 class Stresstracking
 {
-    function getWeeklyAvg($today){
-        //SELECT AVG(t.stress_factor) AS 'Average' FROM User AS u INNER JOIN Task AS t ON u.UID = t.created_by WHERE u.UID = '292e9f36-b177-4e31-9abb-2dfd54aa1904' AND t.end_date >= ('2022-11-28'-7) AND t.end_date <= '2022-11-28'
-        //SELECT AVG(t.stress_factor) AS 'Average' FROM User AS u INNER JOIN Task AS t ON u.UID = t.created_by WHERE u.UID = '292e9f36-b177-4e31-9abb-2dfd54aa1904' AND t.end_date >= DATE_SUB("2022-11-28", INTERVAL 7 DAY) AND t.end_date <= '2022-11-28'
+    function getWeeklyAvg($today): bool|array
+    {
         $tomorrow = $today + 1;
         $stmt = Database::getDb()->prepare("SELECT ROUND(AVG(t.stress_factor),2) AS 'Average' FROM User AS u INNER JOIN Task AS t ON u.UID = t.created_by WHERE u.UID = :UID AND t.end_date >= DATE_SUB(:today, INTERVAL 7 DAY) AND t.end_date < :tomorrow");
         $stmt->bindValue(":UID", $_SESSION['UID']);
@@ -17,17 +16,6 @@ class Stresstracking
 
         return $result;
     }
-
-    /*function getDailyAvg(){
-        $stmt = Database::getDb()->prepare("SELECT SUM(stress_factor) AS 'Sum' FROM Task WHERE created_by=:UID AND DATE(end_date)= CURRENT_DATE LIMIT 1");
-        $stmt->bindValue(":UID", $_SESSION['UID']);
-
-        $stmt->execute();
-
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        return $result;
-    }*/
 
     function getDailyStresslevel(){
         $stmt = Database::getDb()->prepare("SELECT daily_stresslevel FROM User WHERE UID=:UID LIMIT 1");
@@ -51,7 +39,7 @@ class Stresstracking
         return $result;
     }
 
-    function updateDailyStresslevel($daily_stresslevel)
+    function updateDailyStresslevel($daily_stresslevel): bool
     {
         $stmt1 = Database::getDb()->prepare("SELECT daily_stresslevel FROM User WHERE UID=:UID LIMIT 1");
         $stmt1->bindValue(":UID", $_SESSION['UID']);
@@ -68,7 +56,8 @@ class Stresstracking
         else return false;
     }
 
-    function resetDailyStresslevel($daily_stresslevel){
+    function resetDailyStresslevel($daily_stresslevel): bool
+    {
         $stmt = Database::getDb()->prepare("UPDATE User SET daily_stresslevel=:daily_stresslevel WHERE UID=:UID LIMIT 1");
         $stmt->bindValue(":UID", $_SESSION['UID']);
         $stmt->bindValue(":daily_stresslevel", (int)$daily_stresslevel);
