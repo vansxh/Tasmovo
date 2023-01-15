67db8ec08ebd772af41dde738231c7e711f424e3
mt201064 <mt201064@fhstp.ac.at>
2023-01-15 16:40:27 +0100

    code cleanup visualization class

--- Backend/classes/Visualization.class.php
+++ Backend/classes/Visualization.class.php
@@ -3,11 +3,10 @@
 class Visualization
 {
 
-    function getTaskExpenses($userID)
+    function getTaskExpenses($userID): bool|array
     {
         $stmt = Database::getDb()->prepare("SELECT CAST(end_date as DATE) as 'date', expenseID, COUNT(TAID) as 'number' FROM Task WHERE created_by=:userID AND end_date BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) and NOW() GROUP BY CAST(end_date as DATE), expenseID ORDER BY date ASC");
         $stmt->bindValue(":userID", $userID);
-
         $stmt->execute();
 
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
